497c525a84465d4945db7181bd9468de4aa58f6c
Igor Teixeira <igorxteixeira@gmail.com>
2025-10-06 18:09:23 -0300

    first customization

--- lua/custom/plugins/colorizer.lua
+++ lua/custom/plugins/colorizer.lua
@@ -2,4 +2,12 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+
+  { -- Color hexadecimal strings
+    'norcalli/nvim-colorizer.lua',
+    config = function()
+      require('colorizer').setup()
+    end,
+  },
+}
